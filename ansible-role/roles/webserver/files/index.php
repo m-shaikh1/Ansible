7263ef951744b780f2dc2c4751441a390f919308
m-shaikh1 <mohammed.y.shaikh1@gmail.com>
2022-09-21 09:48:24 +0300

    First Commit

---
+++ ansible-role/roles/webserver/files/index.php
@@ -0,0 +1,16 @@
+<?php
+
+ echo "<h1>Hello, World! This is my Ansible page.</h1>";
+
+?>
+<html>
+
+<head><title>Configuration Management Hands On</title></head>
+
+ 
+
+<h1>This server was provisioned using <strong>Ansible</strong></h1>
+
+ 
+
+</html>
